e20c6231667370022ec2b94d5963a812501f1ea5
vk399-github <vikasjawaji007@gmail.com>
2021-06-02 10:42:15 +0530

    JNKS DMO

--- src/main/java/com/employee/EmployeeApplication.java
+++ src/main/java/com/employee/EmployeeApplication.java
@@ -2,12 +2,19 @@ package com.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class EmployeeApplication {
+public class EmployeeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EmployeeApplication.class);
 	}
 
 }
